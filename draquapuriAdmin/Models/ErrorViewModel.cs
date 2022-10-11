0bdea572d8756043f992432e69218062b748ac31
SAnand271 <35259064+Deepak2060@users.noreply.github.com>
2022-10-11 15:50:58 +0530

    First Add Comit

---
+++ draquapuriAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace draquapuriAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
