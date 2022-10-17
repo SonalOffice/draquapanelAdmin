150f6abe017441518b7ba70e59e7b6b84f8f2aa9
SonalOffice <sonalpasait@resiliencesoft.com>
2022-10-17 18:27:01 +0530

    firstcommit

---
+++ draquapuriAdmin/Models/Dbcontexts.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace draquapuriAdmin.Models
+{
+    public class Dbcontexts : DbContext
+    {
+        public Dbcontexts(DbContextOptions<Dbcontexts> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Login> Login { get; set; } 
+    }
+}
---
+++ draquapuriAdmin/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace draquapuriAdmin.Models
+{
+    public class Login
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required (ErrorMessage ="Please enter Phone")]
+        public string? Phone { get; set; }
+        [Required(ErrorMessage = "Please enter Password")]
+        public string? Password { get; set; }
+    }
+}
